18c9e35781b0aeee133686804441a846ef36d882
sio2k1 <test@star-s.ru>
2019-10-18 14:04:14 +1300

    localhost apis

--- my-app/src/api_list/api_articles_axios.js
+++ my-app/src/api_list/api_articles_axios.js
@@ -1,6 +1,6 @@
 // axios cfg file for accessing articles api
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://192.168.1.72:3001/api/articles",
+  baseURL: "http://localhost:3001/api/articles",
   responseType: "json"
 });
--- my-app/src/api_list/api_price_list_axios.js
+++ my-app/src/api_list/api_price_list_axios.js
@@ -1,7 +1,7 @@
 // axios cfg file for accessing price list api
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://192.168.1.72:3001/api/pricelist",
+  baseURL: "http://localhost:3001/api/pricelist",
   responseType: "json"
 });
 //baseURL: "http://localhost:3001/api/pricelist",
